ab89f8b2e4f3eeb944e840ef58681d1666bd5f4a
lizhipeng <lizhipeng@ejzhi.com>
2017-09-06 09:23:39 +0800

    添加pickerView的控件

--- CstomControl/ViewController.m
+++ CstomControl/ViewController.m
@@ -10,8 +10,12 @@
 #import "CustomLabel.h"
 #import "CustomTextField.h"
 #import "CustomButton.h"
+#import "CustomPickerView.h"
 @interface ViewController ()
+{
 
+    CustomPickerView *pickerView;
+}
 @end
 
 @implementation ViewController
@@ -19,17 +23,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    CustomLabel *label=[[CustomLabel alloc]initWithFrame:CGRectMake(100, 100, 200, 40) andSize:14 andTextColor:[UIColor lightGrayColor] andText:@"这是一个测试的label" andTextAlign:NSTextAlignmentCenter];
+    CustomLabel *label=[[CustomLabel alloc]initWithFrame:CGRectMake(100, 50, 200, 40) andSize:14 andTextColor:[UIColor lightGrayColor] andText:@"这是一个测试的label" andTextAlign:NSTextAlignmentCenter];
     [self.view addSubview:label];
     
-    CustomTextField *textField=[[CustomTextField alloc]initWithFrame:CGRectMake(100, 150, 200, 50) andTextSize:16 andTextColor:[UIColor blackColor] andBackGroundColor:[UIColor lightGrayColor] andPlaceolder:@"这是一个测试的textField"];
+    CustomTextField *textField=[[CustomTextField alloc]initWithFrame:CGRectMake(100, 100, 200, 50) andTextSize:16 andTextColor:[UIColor blackColor] andBackGroundColor:[UIColor lightGrayColor] andPlaceolder:@"这是一个测试的textField"];
     [self.view addSubview:textField];
     
-    CustomButton *button=[[CustomButton alloc]initWithFrame:CGRectMake(100, 250, 200, 40) andTitle:@"button" andTextFont:17 andTextColor:[UIColor whiteColor] andBackGroundColor:[UIColor lightGrayColor] andTextAlignment:UIControlContentHorizontalAlignmentCenter];
+    CustomButton *button=[[CustomButton alloc]initWithFrame:CGRectMake(100, 200, 200, 40) andTitle:@"button" andTextFont:17 andTextColor:[UIColor whiteColor] andBackGroundColor:[UIColor lightGrayColor] andTextAlignment:UIControlContentHorizontalAlignmentCenter];
+    [button addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:button];
     
     
     
+}
+
+- (void)btnClick{
+  pickerView=[[CustomPickerView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) andArray:@[@"1",@"2",@"3",@"4"]];
+    pickerView.backgroundColor=[UIColor whiteColor];
+    [self.view addSubview:pickerView];
+
 }
 
 
